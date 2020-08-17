3fa7ca66f0260fae070aabc162a622020f5a2c6b
naman349 <mnaman42@gmail.com>
2020-08-17 12:08:15 +0530

    checlpoint 12

---
+++ scripts/list.js
@@ -0,0 +1,3 @@
+const API_URL = "https://tripadvisor1.p.rapidapi.com/";
+const tripAdvisorHost = "tripadvisor1.p.rapidapi.com";
+const tripAdvisorKey = "e4e9cbf51dmsh46fa155194d0c90p16af78jsn90187b236366";
